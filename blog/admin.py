c8a21ba0da80a08b56bef4109449fb4450a660f6
LeShine <358368802@qq.com>
2018-06-21 23:45:33 +0800

    model

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
-from . models import Article
+from .models import Article
 
-admin.site.register(Article)
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content')
+    # list_filter = ('pub_time',)
+
+
+admin.site.register(Article, ArticleAdmin)
 
--- blog/views.py
+++ blog/views.py
@@ -8,8 +8,34 @@ def index(request):
     return render(request, 'blog/index.html', {'articles': article})
 
 
-def article_page(request,article_id):
+def article_page(request, article_id):
     article = models.Article.objects.get(pk=article_id)
-    return render(request,'blog/article_page.html',{"article":article})
+    return render(request, 'blog/article_page.html', {"article": article})
+
+
+def edit_page(request, article_id):
+    if str(article_id) == '0':
+        return render(request, 'blog/edit_page.html')
+    article = models.Article.objects.get(pk=article_id)
+    return render(request, 'blog/edit_page.html', {"article": article})
+
+
+def edit_action(request):
+    title = request.POST.get('title', 'TITLE')
+    content = request.POST.get('content', 'CONTENT')
+    article_id = request.POST.get('article_id', '0')
+    if article_id == '0':
+        models.Article.objects.create(title=title, content=content)
+        article = models.Article.objects.all()
+        return render(request, 'blog/index.html', {'articles': article})
+    article = models.Article.objects.get(pk=article_id)
+    article.title = title
+    article.content = content
+    article.save()
+    return render(request, 'blog/article_page.html', {"article": article})
+
+
+
+
 
 

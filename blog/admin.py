d24bacaecf2ca0326d211b6e936e68b96a877ebe
LeShine <358368802@qq.com>
2018-06-22 00:00:07 +0800

    model

--- blog/admin.py
+++ blog/admin.py
@@ -5,7 +5,7 @@ from .models import Article
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ('title', 'content')
     # list_filter = ('pub_time',)
-
+#ceshi
 
 admin.site.register(Article, ArticleAdmin)
 
